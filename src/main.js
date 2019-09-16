130f3c7ca2508d55f623e30ef5b14a16fcd5c8d6
王侨文 <wangqiaowen@itcast.cn>
2019-09-16 16:47:23 +0800

    day01

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 // 优先查看文件，如果文件找不到优先找到目录。
 // 如果找到目录， 优先找目录中的index.js
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
