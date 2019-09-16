6500b9ce720fcbaf5fbbeb87167b631317267676
王侨文 <wangqiaowen@itcast.cn>
2019-09-16 17:33:43 +0800

    home and index

---
+++ src/components/home/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>home/index</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Home from '../components/home'
+import Login from '../components/login'
+
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/login',
+      component: Login
+    }
+  ]
 })
